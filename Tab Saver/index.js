27173b8ff1611f644619e592d5ed001cd6e73f00
El-gatto <108247745+elvinny-is-coding@users.noreply.github.com>
2023-12-05 17:55:07 +0800

    Updated code

--- Tab Saver/index.js
+++ Tab Saver/index.js
@@ -2,23 +2,44 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-localStorage.setItem("myLeads", )
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  window.localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
-  renderLeads();
+  render(myLeads);
 });
 
-function renderLeads() {
-  let listItems = "";
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
-  for (let i = 0; i < myLeads.length; i++) {
+function render(leads) {
+  let listItems = "";
+  window.localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-        <a target="_blank" href="${myLeads[i]}">
-          ${myLeads[i]}
+        <a target="_blank" href="${leads[i]}">
+          ${leads[i]}
         </a>
     </li>
     `;
